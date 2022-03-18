2a4aeafa53b7072584bb43677ac7f4e95a56af3a
Erickson-scarlet <iregierick46@gmail.com>
2022-03-18 01:40:45 -0700

    checking for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper  a function that checks for uppercase character
+ * _isupper - a function that checks for uppercase character
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
